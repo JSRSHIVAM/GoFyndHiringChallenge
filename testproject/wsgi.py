a59256cbb0ff0d7e8bb20d2c4a5df70e03df4ae3
tracebackerror <shivam9172@gmail.com>
2017-11-26 01:45:24 +0530

    Heruko Updated

--- testproject/wsgi.py
+++ testproject/wsgi.py
@@ -15,4 +15,7 @@ from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+
